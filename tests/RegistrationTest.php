5b154831ffccc140cd2c2e9276e8b0f9a7208628
Zeeshan Sattar <zeeshan@virtuecenter.com>
2014-04-30 18:21:50 -0400

    eventfindbyslug

--- tests/RegistrationTest.php
+++ tests/RegistrationTest.php
@@ -4,6 +4,7 @@ namespace Opine;
 class RegistrationTest extends \PHPUnit_Framework_TestCase {
     private $registration;
     private $db;
+    private $eventID = '5314f7553698bb5228b15cc2';
 
     public function setup () {
         date_default_timezone_set('UTC');
@@ -11,14 +12,17 @@ class RegistrationTest extends \PHPUnit_Framework_TestCase {
         $container = new Container($root, $root . '/container.yml');
         $this->registration = $container->registration;
         $this->db = $container->db;
+        $this->eventID = new \MongoId($this->eventID);
 
         //eventbyslug//
-    $this->db->collection('events')->update(
-        ['_id' => $this->eventsID],
-        [
-            '_id' => $this->eventsID,
-            'code_name' => 'event']
-            );
+        $this->db->collection('events')->update(
+            ['_id' => $this->eventID],
+            [
+                '_id' => $this->eventID,
+                'code_name' => 'this-event'
+            ],
+            ['upsert' => true]
+        );
 
     }
 
@@ -26,10 +30,9 @@ class RegistrationTest extends \PHPUnit_Framework_TestCase {
         $this->assertTrue(true);
     }
 
-     public function testEventBySlug () {
-        $result = $this->event->code_name($this->event);
-        $this->assertTrue($result);
+    public function testEventFindBySlug () {
+        $result = $this->registration->eventFindBySlug($this->eventID);
+        $this->assertFalse($result);
     }
-
 
 }
